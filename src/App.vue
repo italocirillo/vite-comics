a5e155aae49c0fb1f65894b4289f538243a48108
italocirillo <italocirillo@gmail.com>
2023-04-13 18:58:59 +0200

    PROGETTO IMPOSTATO

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
 
 }
 </script>
 
 <template>
-  <h1>Dc comics</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 
